58b79af5aebeda052c0f0bb81cb248f1c745f032
Jakub89151 <wrx89151@student.wroclaw.merito.pl>
2024-04-18 01:11:52 +0200

    Funkcja srednia dodana poprawnie

--- avg.php
+++ avg.php
@@ -1,14 +1,7 @@
 <?php
 
-	// pamiętaj o zapisaniu pliku (Crtl+S)
-	
 	function srednia(int a, int b)
 	{
 		return (a+b)/2;
-	}
-	
-	function wazona_srednia(int a, int b)
-	{
-		return (3*a+7*b)/10;
 	}
 ?>
